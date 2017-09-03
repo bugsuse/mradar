8ca728ca1c79191cf0f3ddceacf05f12cb93c54e
bugsuse <libravo@foxmail.com>
2017-09-03 09:53:14 +0800

    Update test scripts

--- test/cband_radial_velocity.m
+++ test/cband_radial_velocity.m
@@ -1,30 +1,23 @@
-
 clear,clc
 
 dbstop if error
 tic
-collev = [ 255,255,255; 0,236,236; 0,160,246; 0,0,246; 0,255,0; ...
-           0,200,0; 0,144,0; 255,255,0; 231,192,0; 255,0,0; ...
-           214,0,0; 192,0,0; 255,0,255; 153,85,201 ]/255.;
 
 filename = 'data/NUIST.20140928.070704.AR2';
        
-radar = read_cradar(filename, 2);     
+% 读取径向速度
+radar = read_cradar(filename, 3);     
 
 toc       
 lat = radar.coordinate.elevation(1).latitude.data;
 lon = radar.coordinate.elevation(1).longitude.data;
 height1 = radar.coordinate.elevation(1).height.data;
 prod = radar.products.elevation(1).data;
-% prod(prod < 0) = 0;
 
-figure
 pcolor(lon, lat, prod)
 axis square        %  保持绘图框为正方形
 shading flat       %  去除图形网格线
 cid = colorbar;
-caxis([0, 70])
-colormap(collev)
 
 se = ginput(2);
 
@@ -48,6 +41,3 @@ ylim([0, 20])
 axis square        %  保持绘图框为正方形
 shading flat       %  去除图形网格线
 cid = colorbar;
-caxis([0, 70])
-colormap(collev);
-
--- test/sband_radial_velocity.m
+++ test/sband_radial_velocity.m
@@ -1,30 +1,36 @@
-
 clear,clc
 
 dbstop if error
-tic
-collev = [ 255,255,255; 0,236,236; 0,160,246; 0,0,246; 0,255,0; ...
-           0,200,0; 0,144,0; 255,255,0; 231,192,0; 255,0,0; ...
-           214,0,0; 192,0,0; 255,0,255; 153,85,201 ]/255.;
 
-filename = 'data/NUIST.20140928.070704.AR2';
-       
-radar = read_cradar(filename, 2);     
+filename = 'data/SA_CAP.bin';
 
-toc       
+% 读取径向速度
+types = 2; % 
+lon = 120.2011;
+lat = 33.4311; % 雷达经纬度坐标
+
+radar = read_sradar(filename, types, lon, lat, 0);
+
 lat = radar.coordinate.elevation(1).latitude.data;
 lon = radar.coordinate.elevation(1).longitude.data;
 height1 = radar.coordinate.elevation(1).height.data;
 prod = radar.products.elevation(1).data;
-% prod(prod < 0) = 0;
+phinum = radar.info.elenum;
+
+dbz = zeros(size(prod, 1), size(prod, 2), phinum);
+z = dbz;
+dbz(:, :, 1) = prod;
+z(:, :, 1) = height1;
 
 figure
 pcolor(lon, lat, prod)
 axis square        %  保持绘图框为正方形
 shading flat       %  去除图形网格线
 cid = colorbar;
-caxis([0, 70])
-colormap(collev)
+%   set grid line style for colorbar to solid line
+%   set(cid, 'YGrid', 'on', 'GridLineStyle', '-');
+xlim([119.3, 120.5])
+ylim([33.4, 34.6])
 
 se = ginput(2);
 
@@ -35,19 +41,24 @@ interp = 'se';
 
 method = 'nearest';
 
+types = 1;
 step = 0.01; % 控制经度数据插值
 itpstep = 0.01; % 控制高度插值间隔
 
 [itpprod, itpheight, itplon, itplat] = cross_section_ppi(radar, interp, 'stapos', stapos, 'endpos', endpos, 'hor', step, 'ver', itpstep, 'method', 'nearest');
 
 figure
-%axis([min(min(dist)) max(max(dist)) 0 20])
 pcolor(itplon, itpheight, itpprod)
 ylabel('Height (km)')
+ylim([0, 20])
+%axis square        %  保持绘图框为正方形
+shading flat       %  去除图形网格线
+cid = colorbar;
+
+%% ppi to rhi
+azimu = 314;
+[prod, height, lon, lat] = ppi_to_rhi(radar, azimu);
 ylim([0, 20])
 axis square        %  保持绘图框为正方形
 shading flat       %  去除图形网格线
 cid = colorbar;
-caxis([0, 70])
-colormap(collev);
-
